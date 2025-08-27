08978a69753101c8bda446b68312b452998ff405
XIO <100460377+TazkieCT@users.noreply.github.com>
2025-08-27 08:53:56 +0700

    feat: Implement Text-to-Speech and update UI
    This commit introduces Text-to-Speech (TTS) functionality to the MainActivity and updates the layout.
    
    Key changes:
    - Integrated `TextToSpeech` in `MainActivity` to speak text.
    - Implemented `TextToSpeech.OnInitListener` to handle TTS initialization and language support.
    - Added a `speak()` method for TTS output.
    - Enabled view binding in `build.gradle.kts`.
    - Updated `activity_main.xml`:
        - Replaced the initial `TextView` with a `LinearLayout` containing a `TextView` to display text.
        - Added a `GridLayout` with six `Button`s.
        - Applied padding to the root `ConstraintLayout`.
    - Initialized button variables in `MainActivity` (though they are not yet used).

--- app/src/main/java/com/example/brailly/MainActivity.kt
+++ app/src/main/java/com/example/brailly/MainActivity.kt
@@ -1,14 +1,50 @@
 package com.example.brailly
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.brailly.databinding.ActivityMainBinding
+import java.util.Locale
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    private lateinit var tts: TextToSpeech
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        tts = TextToSpeech(this, this)
+
+        var value1 = binding.button1
+        var value2 = binding.button2
+        var value3 = binding.button3
+        var value4 = binding.button4
+        var value5 = binding.button5
+        var value6 = binding.button6
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts.setLanguage(Locale("id", "ID"))
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                println("Bahasa tidak didukung")
+            } else {
+                speak("Hello!")
+            }
+        } else {
+            println("Inisialisasi TTS gagal")
+        }
+
+    }
+
+    fun speak(text: String) {
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null)
     }
 }
