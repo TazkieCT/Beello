aad516f3d052728b79cba29e35e032119f29429b
XIO <100460377+TazkieCT@users.noreply.github.com>
2025-08-29 19:05:56 +0700

    feat: Add LandingActivity and logo
    This commit introduces a new `LandingActivity` which will serve as the initial screen of the app, and includes a logo asset.
    
    Key changes:
    - `LandingActivity.kt`:
        - Created a new `AppCompatActivity` named `LandingActivity`.
        - Sets its content view to `R.layout.activity_landing`.
    - `activity_landing.xml`:
        - Added a new layout file for `LandingActivity`.
        - It's a `ConstraintLayout` with a background color set to `@color/colorSelected`.
    - `AndroidManifest.xml`:
        - Declared `LandingActivity` and set it as the launcher activity.
        - Changed `MainActivity` to `android:exported="false"` as it's no longer the entry point.
    - `drawable/logo.png`:
        - Added a new image asset for the application logo.

---
+++ app/src/main/java/com/example/brailly/LandingActivity.kt
@@ -0,0 +1,14 @@
+package com.example.brailly
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LandingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+    }
+}
